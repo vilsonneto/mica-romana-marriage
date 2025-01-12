00bec74c530d48e82a0a086395a3e57ada05027d
vilsonpneto <vilson.neto57@gmail.com>
2025-01-12 03:16:37 -0300

    feat(qrcode): adicionado qrcode para confirmacao de presenca

--- src/app/registry/page.tsx
+++ src/app/registry/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Subtitle } from "@/components/Subtitle";
+import Image from "next/image";
 
 export default function RegistryPage() {
   return (
@@ -11,10 +12,33 @@ export default function RegistryPage() {
         objectPosition="55%"
         title="Confirmação"
       />
-      <div className="pt-12">
-        Leia o QR Code abaixo para nos enviar uma mensagem no WhatsApp e
-        confirmar sua presença nesse dia tão especial. ❤️
-      </div>
+      <section className="mt-10 flex gap-0 m-auto flex-col items-center w-full">
+        <div className="flex py-6 flex-col justify-end items-center md:w-3/4 w-2/3">
+          <p className="text-black md:text-2xl text-xl md:w-2/3 w-full text-center mb-6">
+            Leia o QR Code abaixo e nos envie uma mensagem para confirmar sua
+            presença nesse dia tão especial.
+          </p>
+          <div className="border-black border-solid border-[1px] border-opacity-25 rounded-sm">
+            <Image
+              alt="Character"
+              src="/img/qrcode.png"
+              height={478}
+              width={240}
+            />
+          </div>
+          <p className="text-black md:text-2xl text-xl md:w-2/3 w-full text-center mt-6">
+            Você também pode confirmar sua presença{" "}
+            <a
+              href="https://w.app/gjo3Rn"
+              target="_blank"
+              className="font-bold underline"
+            >
+              clicando aqui
+            </a>
+            .
+          </p>
+        </div>
+      </section>
     </>
   );
 }
